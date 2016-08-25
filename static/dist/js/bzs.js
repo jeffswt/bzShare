75e98c190953a29c047e52006204d555b1d7645e
Geoffrey, Tang <ht35268@126.com>
2016-08-25 22:31:02 +0800

    Minor tweaks before server implementation.

--- static/dist/js/bzs.js
+++ static/dist/js/bzs.js
@@ -14,6 +14,10 @@ if (typeof jQuery === "undefined") {
   throw new Error("bzShare requires jQuery");
 }
 
+var error_page_404 = "<section class=\"content\"><div class=\"error-page\"><h2 class=\"headline text-yellow\"> 404</h2><div class=\"error-content\">  <h3><i class=\"fa fa-warning text-yellow\"></i> Oops! Page not found.</h3><p>We could not find the page you were looking for. Meanwhile, you may <a href=\"javascript:bzsModifyMainFrame('/home')\">return to home</a> or try not using the search form.</p><form class=\"search-form\"><div class=\"input-group\"><input type=\"text\" disabled=true name=\"search\" class=\"form-control\" placeholder=\"Search bzShare\"><div class=\"input-group-btn\"><button type=\"submit\" name=\"\" class=\"btn btn-warning btn-flat\"><i class=\"fa fa-search\"></i></button></div></div></form></div></div></section>";
+
+var bzsModifyMainFrame_last = ''
+
 /* bzsModifyMainFrame
  * ==========
  * Changes the main frame into given target.
@@ -23,6 +27,9 @@ if (typeof jQuery === "undefined") {
  */
 var bzsModifyMainFrame = function(address)
 {
+  if (bzsModifyMainFrame_last == address) {
+	return ;
+  }
   var xml_request = new XMLHttpRequest();
   var mainframe_body = document.getElementById('container-mainframe')
   xml_request.open('html', address, false, '', '');
@@ -31,12 +38,13 @@ var bzsModifyMainFrame = function(address)
   } catch (except) {}
   var __container_mainframe_modify_proc_2 = function() {
     if (!xml_request.responseText) {
-      mainframe_body.innerHTML = "404 ERROR";
+      mainframe_body.innerHTML = error_page_404;
     } else {
       mainframe_body.innerHTML = xml_request.responseText;
+	  bzsModifyMainFrame_last = address; // Setting for not clicking too much.
     }
-    mainframe_body.setAttribute('class', 'animated fadeInRight');
+	mainframe_body.setAttribute('class', 'animated fadeInRight');
   };
   mainframe_body.setAttribute('class', 'animated fadeOutLeft');
-  setTimeout(__container_mainframe_modify_proc_2, 500);
+  setTimeout(__container_mainframe_modify_proc_2, 800);
 };
