8827f9bd35fa136dd455b3523b2ef5cc132c8834
Geoffrey, Tang <jeffswt@outlook.com>
2016-08-29 22:41:41 +0800

    Initially implemented SQLFS (untested!)
    * Initial implementation of SQLFS, but has not been tested at all. The
    code is so big that I daren't test it at all, but I would like to try
    and it is inevitable.

--- bzs/const.py
+++ bzs/const.py
@@ -2,6 +2,11 @@
 universal_options_list = {
     'author': 'Geoffrey, Tang.',
     'copyright': 'Copyright 2016, Geoffrey Tang. All lefts reversed.',
+    'db-name': 'postgres',
+    'db-user': 'root',
+    'db-password': '123456',
+    'db-host-addr': '127.0.0.1',
+    'db-host-port': '8079',
     'license': 'GNU GPL v3',
     'server-name': 'Apache/2.4.1 (Linux 2.6.32)',
     'time-zone': 'Asia/Shanghai',
